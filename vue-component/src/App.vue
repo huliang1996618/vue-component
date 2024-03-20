51c22274a9487bc0afa570d714babb2aad6dda87
earlhu <earlhu@tencent.com>
2024-03-20 20:53:34 +0800

    固定长度列表项的虚拟列表

--- vue-component/src/App.vue
+++ vue-component/src/App.vue
@@ -1,88 +1,39 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import MyWatermark from './components/watermark/MyWatermark.vue';
-</script>
-
 <template>
-  <MyWatermark content="arco.design">
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</MyWatermark>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+  <div id="app">
+    <VirtualList :listData="data" :itemSize="100"/>
+  </div>
+  </template>
+  
+<script lang="ts">
+import VirtualList from './components/virtual-list/VirtualList.vue';
+let d = [];
+for (let i = 0; i < 1000; i++) {
+  d.push({ id: i, value: i });
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  
+export default {
+  name: "App",
+  data() {
+    return {
+      data: d
+    };
+  },
+  components: {
+    VirtualList
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+};
+</script>
+  
+  <style>
+  html{
+    height: 100%;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  body{
+    height: 100%;
+    margin:0;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  #app{
+    height:100%;
   }
-}
-</style>
+  </style>
+  
--- vue-component/src/views/HomeView.vue
+++ vue-component/src/views/HomeView.vue
@@ -1,9 +1,28 @@
+<template>
+  <div id="app1">
+    <VirtualList :list-data="data" :item-size="100" />
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import VirtualList from '@/components/virtual-list/VirtualList.vue';
+import { ref } from 'vue';
+const data = ref<any>([]);
+
+for(let i = 0; i < 10000; i++) {
+  data.value.push({ id: i, value: i });
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+html{
+  height: 100%;
+}
+body{
+  height: 100%;
+  margin:0;
+}
+#app1{
+  height:100%;
+}
+</style>
